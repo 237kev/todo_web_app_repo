ba8a0b79f4a7ea4276b990e6a3e8a9d3727ebfed
Kevin <k3v1n.k4md3m@gmail.com>
2025-02-26 14:44:46 +0100

    add and delete function has been implemented and it works fine

--- web_todo_app.py
+++ web_todo_app.py
@@ -1,12 +1,26 @@
 import streamlit as st
 import functions
 
+todos = functions.get_todos(functions.TODO_FILES_PATH)
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.set_todos(functions.TODO_FILES_PATH, todos)
+
+
+
 st.title('My todo Webapp')
 st.subheader('This is my todo webapp')
 st.write('This app should increase you productivity')
 
-todos = functions.get_todos(functions.TODO_FILES_PATH)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.set_todos(functions.TODO_FILES_PATH, todos)
+        del st.session_state[todo]
+        st.rerun()
 
-
-for todo in todos:
-    st.checkbox(todo)
+st.text_input(label='New todo', placeholder='enter a new todo item ...',
+              on_change=add_todo, key='new_todo')
